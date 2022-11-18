775435a04c3983f29310a578c1721a404e8d2197
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2022-11-18 19:19:09 +0530

    CHANGE LOG: - Replaced browser router with Hash Router

--- Frontend/mess-management-system-ui/src/index.js
+++ Frontend/mess-management-system-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./store/store";
 import { Provider } from "react-redux";
 import App from "./App";
@@ -9,10 +9,10 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
